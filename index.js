ab8e62f67a2ab604b42aaea8d383ebee8de08c3a
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-07-24 03:23:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class Formatter {
   static capitalize(string){
-    string.toUpperCase();
+    return string.toUpperCase();
   }
   
   static sanitize(string){
-    string.replace(/[^A-Za-z0-9-' ]+/g, '' )
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '' )
   }
 }
